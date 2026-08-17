f30b80916183b9ab97a655ad9dfa5f01b794c264
Rio Adrian <062340833237@student.polsri.ac.id>
2026-08-17 21:08:08 +0700

    add instances-container dependency and refactor container registration for user and password hash use cases

--- src/Infrastructures/container.js
+++ src/Infrastructures/container.js
@@ -1,6 +1,13 @@
+import { createContainer } from 'instances-container';
+import { nanoid } from 'nanoid';
+import bcrypt from 'bcrypt';
+import pool from './database/postgres/pool.js';
+
+import UserRepository from '../Domains/users/UserRepository.js';
 import UserRepositoryPostgres from './repositories/UserRepositoryPostgres.js';
 import AuthenticationRepositoryPostgres from './repositories/AuthenticationRepositoryPostgres.js';
 import BcryptPasswordHash from './security/BcryptPasswordHash.js';
+import PasswordHash from '../Applications/security/PasswordHash.js';
 import JwtTokenManager from './security/JwtTokenManager.js';
 import AddUserUseCase from '../Applications/use_case/AddUserUseCase.js';
 import LoginUserUseCase from '../Applications/use_case/LoginUserUseCase.js';
@@ -10,47 +17,62 @@ import DetailUserUseCase from '../Applications/use_case/DetailUserUseCase.js';
 import UpdateFullnameUseCase from '../Applications/use_case/UpdateFullnameUseCase.js';
 import DeleteUserUseCase from '../Applications/use_case/DeleteUserUseCase.js';
 
+const container = createContainer();
+
 const userRepository = new UserRepositoryPostgres();
 const authenticationRepository = new AuthenticationRepositoryPostgres();
 const passwordHash = new BcryptPasswordHash();
 const tokenManager = new JwtTokenManager();
 
-const container = {
-    getInstance(key) {
-        switch(key) {
-            case 'AddUserUseCase':
-                return new AddUserUseCase({userRepository, passwordHash});
+container.register([
+    {
+        key: UserRepository.name,
+        Class: UserRepositoryPostgres,
+        parameter: {
+            dependencies: [
+                {
+                    concrete: pool,
+                },
+                {
+                    concrete: nanoid,
+                },
+            ],
+        },
+    },
+    {
+        key: PasswordHash.name,
+        Class: BcryptPasswordHash,
+        parameter: {
+            dependencies: [
+                {
+                    concrete: bcrypt,
+                },
+                {
+                    concrete: nanoid,
+                },
+            ],
+        },
+    }
+]);
 
-            case 'LoginUserUseCase':
-                return new LoginUserUseCase({
-                    userRepository,
-                    authenticationRepository,
-                    tokenManager,
-                    passwordHash
-                });
-
-            case 'LogoutUserUseCase':
-                return new LogoutUserUseCase({ authenticationRepository });
-
-            case 'RefreshAuthenticationUseCase':
-                return new RefreshAuthenticationUseCase({
-                    authenticationRepository,
-                    tokenManager,
-                });
-
-            case 'DetailUserUseCase':
-                return new DetailUserUseCase({ userRepository });
-
-            case 'UpdateFullnameUseCase':
-                return new UpdateFullnameUseCase({ userRepository });
-
-            case 'DeleteUserUseCase':
-                return new DeleteUserUseCase({ userRepository, authenticationRepository });
-
-            default:
-                throw new Error(`Use case ${key.name} tidak ditemukan di container`);
+container.register([
+    {
+        key: AddUserUseCase.name,
+        Class: AddUserUseCase,
+        parameter: {
+            injectType: 'destructuring',
+            dependencies: [
+                {
+                    name: 'userRepository',
+                    internal: UserRepository.name
+                },
+                {
+                    name: 'passwordHash',
+                    internal: PasswordHash.name
+                }
+            ]
         }
     }
-}
+])
 
 export default container;
