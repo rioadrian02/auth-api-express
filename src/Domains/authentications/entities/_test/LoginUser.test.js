b58e1f901ad90c1bd7ce64e1bab48118b09decb2
Rio Adrian <062340833237@student.polsri.ac.id>
2026-08-18 09:03:27 +0700

    add tests for LoginUser class to validate required properties and data types

--- src/Domains/authentications/entities/_test/LoginUser.test.js
+++ src/Domains/authentications/entities/_test/LoginUser.test.js
@@ -1,5 +1,5 @@
 import { jest } from '@jest/globals';
-import LoginUser from '../users/LoginUser.js';
+import LoginUser from '../LoginUser.js';
 
 describe('LoginUser', () => {
     test('harus error jika property tidak lengkap', () => {
